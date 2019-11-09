1e4b0d67610e6d0e7557d60043b39921869027e9
cxx <841298673@qq.com>
2019-11-09 20:29:14 +0800

    1

---
+++ src/main/java/com/apesource/Spring_Boot.java
@@ -0,0 +1,11 @@
+package com.apesource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring_Boot {
+    public static void main(String[] args) {
+        SpringApplication.run(Spring_Boot.class,args);
+    }
+}
